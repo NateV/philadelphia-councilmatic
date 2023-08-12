ad430f7ed0d93f3527f06eed42742d79308ecfc3
Nate Vogel <nathan.c.vogel@gmail.com>
2023-08-12 16:20:41 -0400

    now stuck with updating db because of some issue with pseudo ids for database entries

--- philadelphia_scraper/bills.py
+++ philadelphia_scraper/bills.py
@@ -1,10 +1,17 @@
 from typing import Dict, Tuple, Generator, List
 from pupa.scrape import Scraper
+import json
 from pupa.scrape import Bill, VoteEvent
 from pupa.scrape.bill import Action
 from legistar.bills import LegistarBillScraper
 from datetime import date
 import datetime
+import re
+
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
     
@@ -13,8 +20,8 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
     BASE_WEB_URL = "https://phila.legistar.com/"
     TIMEZONE = "US/Eastern"
     VOTE_OPTIONS = {
-        "Ayes":"yes",
-        "Nayes":"no"
+        "ayes":"yes",
+        "nays":"no"
             }
 
     def actions(self, matter_url: str) -> Generator[Tuple[Dict[str, str], Tuple[dict | None, dict | None]], None, None]:
@@ -50,11 +57,100 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
                 # this ends up repeating (result, (result, {votes})). because extractVotes returns (result, {votes}). not sure we want to.
                 #votes = (result, self.extractVotes(tally_url) if tally_url is not None else None)
                 votes = self.extractVotes(tally_url) if tally_url is not None else None
-            else:
-                print("tally is ", tally)
             
 
             yield bill_action, votes
+
+    def clean_action_classification(self, action: str | None) -> str:
+        """
+        Map Philly's Action values to the schema's classification types.
+
+        See https://opencivicdata.info/en/latest/scrape/bills.html#scraping-votes
+        Bill actions should be one of the following:
+
+            
+
+        filing
+
+        introduction
+
+        reading-1
+
+        reading-2
+
+        reading-3
+
+        passage
+
+        failure
+
+        withdrawal
+
+        substitution
+
+        amendment-introduction
+
+        amendment-passage
+
+        amendment-withdrawal
+
+        amendment-failure
+
+        amendment-amendment
+
+        committee-passage
+
+        committee-passage-favorable
+
+        committee-passage-unfavorable
+
+        committee-failure
+
+        executive-receipt
+
+        executive-signature
+
+        executive-veto
+
+        executive-veto-line-item
+
+        became-law
+
+        veto-override-passage
+
+        veto-override-failure
+
+        deferral
+
+        receipt
+
+        referral
+
+        referral-committee
+
+
+
+        """
+
+        if action is None:
+            return ""
+
+        classification_map = {
+                'introduced and referred': "introduction",
+                'amended':'amendment-amendment',
+                'reported favorably, rule suspension requested': 'committee-passage-favorable',
+                "read and ordered placed on next week's second reading calendar": 'reading-1',
+                "read and passed":"passage",
+                "signed":"executive-signature",
+                }
+
+        possible_classification = classification_map.get(action.lower())
+
+        if possible_classification is None:
+            logger.warning("Don't know how to classify action - %s", action)
+            return action.lower()
+
+        return possible_classification
 
 
     def scrape(self, search_text: str='qualified electors', created_after: date | None = date(2023,1,1), created_before: date | None = date(2023,1,28) ) -> Generator[Bill | VoteEvent, None, None]:
@@ -78,7 +174,7 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
                 identifier = bill['File\xa0#']
                 title = bill['Title']
                 
-                classification = bill.get('Type').strip().lower()
+                classification = (bill.get('Type') or "").strip().lower()
 
                 date_created = bill["File\xa0Created"]
                 
@@ -98,6 +194,9 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
 
                         # Classification of an action describes the type of the action, like 'filing' or 'executive-signature'
                         # TODO need a dict that maps the philly action's "Action" key to the enum values of councilmatic's classifications.
+
+                        classification = self.clean_action_classification(action.get("action_description"))
+
                         act = bill_.add_action(action['action_description'],
                                               action['action_date'],
                                               organization={'name': action['responsible_org']},
@@ -117,7 +216,7 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
 
     def get_vote_event(self, bill: Bill, act: Action, votes: List[Tuple[str, str]], result: str) -> VoteEvent:
         '''Make VoteEvent object from given Bill, action, votes and result.'''
-        organization = "?"
+        organization = json.loads(act['organization_id'].lstrip('~'))
         vote_event = VoteEvent(legislative_session=bill.legislative_session,
                                motion_text=act['description'],
                                organization=organization,
@@ -126,9 +225,10 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
                                result=result,
                                bill=bill)
 
-        legistar_web = [src['url'] for src in bill.sources]
-
-        vote_event.add_source(legistar_web)
+        legistar_web_list = [src['url'] for src in bill.sources if src.get('note') == 'web']
+        if len(legistar_web_list) > 0:
+            legistar_web = legistar_web_list[0]
+            vote_event.add_source(legistar_web)
 
         for vote in votes:
             raw_option = vote[0].lower()
@@ -138,8 +238,7 @@ class PhiladelphiaBillScraper(LegistarBillScraper, Scraper):
                 continue
 
             
-            clean_option = self.VOTE_OPTIONS.get(raw_option, raw_option)
-            #clean_option = raw_option
+            clean_option = self.VOTE_OPTIONS.get(raw_option.lower(), raw_option.lower())
             vote_event.vote(clean_option, vote[1].strip())
 
         return vote_event
